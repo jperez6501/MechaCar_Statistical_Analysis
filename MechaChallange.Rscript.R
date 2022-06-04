f28097c0a555387595c7df76032440118dc7cdb5
jperez6501 <100246124+jperez6501@users.noreply.github.com>
2022-06-04 14:59:13 -0400

    Add files via upload

--- MechaChallange.Rscript.R
+++ MechaChallange.Rscript.R
@@ -1,5 +1,16 @@
-
+# Deliverable 1
 mecha_car <- read.csv('Desktop/R_Analysis/MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import mecha car dataset
 library(dplyr)
 lm(mpg~vehicle_length + vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,data=mecha_car) #generate multiple linear regression model
 summary(lm(mpg~vehicle_length + vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,data=mecha_car)) #summarize linear model
+
+## Deliverable 2 
+suspension_coil <- read.csv('Desktop/R_Analysis/Suspension_Coil.csv',check.names = F,stringsAsFactors = F) #import suspension coil dataset
+total_summary<-suspension_coil %>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups ='keep') #create summary table
+lot_summary<- suspension_coil  %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups ='keep') #create lot summary table grouped by manufacturing lot  
+
+### Deliverable 3 
+t.test(suspension_coil$PSI, mu=1500) #compare PSI in population means
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
